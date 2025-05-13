015adecf1de6a3d64b0c9004afc84ff42eaf555d
palak garg <gargpalak0401@gmail.com>
2025-05-13 09:41:35 +0530

    files

--- src/model/DBConnection.java
+++ src/model/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/javaproject2";
     private static final String USER = "root";
-    private static final String PASS = "Mahashakti@123";
+    private static final String PASS = "Sunny@41";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
